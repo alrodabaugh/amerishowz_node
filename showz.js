3f21d2491f1fac3ca8ff0150ebd9ef8b63688334
alex.rodabaugh@gmail.com <AmeriSHOWZ@gmail.com>
2016-05-31 00:38:08 -0400

    added ejs, defaults, app.js

---
+++ showz.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+  res.render('default', {
+    title: 'Home'
+  });
+});
+
+app.get('*', function(req, res){
+  res.send('Bad Route');
+});
+
+var server = app.listen(3000, function() {
+  console.log('Listening on port 3000');
+});
